a602d70d675cb6b2dddb9f09616794e09530f986
User <User@DESKTOP-K4C7T1M>
2022-04-27 06:15:05 +0530

    commit1

--- Isa/src/Orange.java
+++ Isa/src/Orange.java
@@ -14,8 +14,5 @@ String color;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Orange greenApple = new Orange("green");
-		String color = greenApple.getColor();
-		System.out.println("color of apple is " + color);
 	}
 }
